c1373e35b530ab41472f2f78e46f7000445d35a7
Amelia-May Crooms <127aacrooms@frhsd.com>
2026-06-04 14:02:40 -0400

    124567

---
+++ script.js
@@ -0,0 +1,63 @@
+let cars = {
+    car1: {
+        make: 1,
+        model: 1,
+        year: 1,
+        category: 1,
+        colour: 1,
+        price: 1,
+        photo: 1,
+        fuelType: 1,
+        horsePower: 1,
+        seatingCapacity: 1
+    },
+ car2: {
+        make: 1,
+        model: 1,
+        year: 1,
+        category: 1,
+        colour: 1,
+        price: 1,
+        photo: 1,
+        fuelType: 1,
+        horsePower: 1,
+        seatingCapacity: 1
+    },
+ car3: {
+        make: 1,
+        model: 1,
+        year: 1,
+        category: 1,
+        colour: 1,
+        price: 1,
+        photo: 1,
+        fuelType: 1,
+        horsePower: 1,
+        seatingCapacity: 1
+    },
+ car4: {
+        make: 1,
+        model: 1,
+        year: 1,
+        category: 1,
+        colour: 1,
+        price: 1,
+        photo: 1,
+        fuelType: 1,
+        horsePower: 1,
+        seatingCapacity: 1
+    },
+ car5: {
+        make: 1,
+        model: 1,
+        year: 1,
+        category: 1,
+        colour: 1,
+        price: 1,
+        photo: 1,
+        fuelType: 1,
+        horsePower: 1,
+        seatingCapacity: 1
+    }
+
+}
