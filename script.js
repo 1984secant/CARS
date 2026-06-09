16e470a304e7e163b6564a3b0abf6de628fb1a89
Amelia-May Crooms <127aacrooms@frhsd.com>
2026-06-09 14:03:07 -0400

    123456

--- script.js
+++ script.js
@@ -8,32 +8,32 @@ let cars = {
         price: "46050",
         photo: 1,
         fuelType: "Gas",
-        horsePower: 1,
-        seatingCapacity: 1
+        horsePower: 250,
+        seatingCapacity: 5
     },
- car2: {
-        make: 1,
-        model: 1,
-        year: 1,
-        category: 1,
-        colour: 1,
-        price: 1,
+ fordEsc: {
+        make: "Ford",
+        model: "U9G",
+        year: 2025,
+        category: "Sport Utility",
+        colour: "Carbonized Gray",
+        price: 33980,
         photo: 1,
-        fuelType: 1,
-        horsePower: 1,
-        seatingCapacity: 1
+        fuelType: "Gas",
+        horsePower: 180,
+        seatingCapacity: 5
     },
- car3: {
-        make: 1,
-        model: 1,
-        year: 1,
+ fordFus: {
+        make: "Ford",
+        model: "P0L",
+        year: 2019,
         category: 1,
-        colour: 1,
-        price: 1,
+        colour: "White",
+        price: "15584",
         photo: 1,
-        fuelType: 1,
-        horsePower: 1,
-        seatingCapacity: 1
+        fuelType: "Gas/Electric Hybrid",
+        horsePower: 195,
+        seatingCapacity: 5
     },
  car4: {
         make: 1,
@@ -60,4 +60,19 @@ let cars = {
         seatingCapacity: 1
     }
 
+}
+
+document.getElementById('fordMav').addEventListener('click', fordMav)
+
+function fordMav(){
+    document.getElementById('make').textContent = cars.fordMav.make; 
+    document.getElementById('model').textContent = cars.fordMav.model;
+    document.getElementById('year').textContent = cars.fordMav.year;
+    document.getElementById('category').textContent = cars.fordMav.category;
+    document.getElementById('color').textContent = cars.fordMav.colour;
+    document.getElementById('price').textContent = cars.fordMav.price;
+    document.getElementById('fuelType').textContent = cars.fordMav.fuelType;
+    document.getElementById('horsePower').textContent = cars.fordMav.horsePower;
+    document.getElementById('seatingCapacity').textContent = cars.fordMav.seatingCapacity;
+    // document.getElementById('').textContent = cars.fordMav.x;
 }
